519e0cb0d4bbba11d6afef53cdc55158f42c214b
Atsushi Ezura <ezura.atsushi@aainc.co.jp>
2024-07-22 17:16:49 +0900

    [change] production code

--- app/Production.php
+++ app/Production.php
@@ -5,3 +5,12 @@ $y = 0;
 $z = 1000;
 echo "x = ${x}, y = ${y}\n";
 echo "z = ${z}\n";
+
+$a = array(
+    'hoge' => 1,
+    'fuga' => 2,
+    'piyo' => 3,
+    'foo'  => 4,
+    'bar'  => 5,
+    'baz'  => 6,
+);
